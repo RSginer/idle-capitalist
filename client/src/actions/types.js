f753d6ad7defd6fc0c184836cc2f6d2394b3c222
Rubén Soler <ruben.soler@exp360.com>
2020-02-21 00:55:39 +0100

    Fix missing ,

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,7 +2,7 @@ export const types = {
   INIT_GAME: '[GAME] Init',
   INIT_GAME_SUCCESS: '[GAME] Init Succesfully',
   INIT_GAME_ERROR: '[GAME] Init Error',
-  BUY_BUSINESS: '[BUSINESS] Buy Business'
-  BUY_BUSINESS_SUCCESS: '[BUSINESS] Bought Business Succesfully'
+  BUY_BUSINESS: '[BUSINESS] Buy Business',
+  BUY_BUSINESS_SUCCESS: '[BUSINESS] Bought Business Succesfully',
   BUY_BUSINESS_ERROR: '[BUSINESS] Bought Business Succesfully'
 }
