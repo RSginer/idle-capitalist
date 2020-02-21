77fa99f3b41bd7962cc0707e08cbbaa862f64147
Rubén Soler <ruben.soler@exp360.com>
2020-02-21 01:02:12 +0100

    Style loader

--- client/src/game/components/loading/index.js
+++ client/src/game/components/loading/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import "./index.css";
+
 export const GameLoading = (props) => {
   return (
-    <div>Loading...</div>
+    <div className="loading-container">
+    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    <span className="loading-label">Loading...</span>
+    </div>
   )
 }
