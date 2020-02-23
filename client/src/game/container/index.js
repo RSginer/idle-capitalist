276312ebda744b6c3ebedf4d1b1330165c2555bd
Rubén Soler <ruben.soler@exp360.com>
2020-02-23 11:32:46 +0100

    Lint

--- client/src/game/container/index.js
+++ client/src/game/container/index.js
@@ -42,6 +42,7 @@ export const Game = () => {
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+ seconds;}
+
     return hours+':'+minutes+':'+seconds;
   }
 
