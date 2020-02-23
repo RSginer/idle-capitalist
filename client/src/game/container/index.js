32962c49f40d12dda50b4e4a37e24a1c3581bf36
Rubén Soler <ruben.soler@exp360.com>
2020-02-23 11:32:59 +0100

    Lint

--- client/src/game/container/index.js
+++ client/src/game/container/index.js
@@ -35,15 +35,15 @@ export const Game = () => {
     }
 
     let secNum = ms / 1000;
-    let hours   = Math.floor(secNum / 3600);
+    let hours = Math.floor(secNum / 3600);
     let minutes = Math.floor((secNum - (hours * 3600)) / 60);
     let seconds = Math.ceil(secNum - (hours * 3600) - (minutes * 60));
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+ seconds;}
+    if (hours < 10) { hours = "0" + hours; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
 
-    return hours+':'+minutes+':'+seconds;
+    return hours + ':' + minutes + ':' + seconds;
   }
 
   return (
